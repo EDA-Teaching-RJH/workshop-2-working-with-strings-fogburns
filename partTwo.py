b118aabe9177fb5e146ecfce18a7a8ac4da96f0f
fogburns <jv299@kent.ac.uk>
2026-01-20 13:28:49 +0000

    done

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,14 @@
 import math  
-
+print("To workout Hypotenuse(C) Enter A and B Below...")
 def main():
-#TO DO  
-
+    global A
+    global B
+    A=float(input("Provide A: "))
+    B=float(input("provide B: "))
 def pythag(A,B):
-#TO DO  
-
+    global C
+    c=(A**2+B**2)
+    C=math.sqrt(c)
+    print("C is :"+str(C))
 main()
+pythag(A,B)
